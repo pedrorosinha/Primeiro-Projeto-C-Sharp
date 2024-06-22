01fdb61a5fed073810715314c004e63476093ad4
pedro felipe <pedrofelipinho@gmail.com>
2024-06-22 20:04:02 -0300

    Criando a base do menu de navegação só com as strings, ainda sem interação

--- PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/Program.cs
@@ -16,4 +16,14 @@ void ExibirMensagemBoasVindas()
     Console.WriteLine(mensagemDeBoasVindas);
 }
 
+void ExibirOpcoesDoMenu()
+{
+    Console.WriteLine("\nDigite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a média de uma banda");
+    Console.WriteLine("digite -1 para sair");
+}
+
 ExibirMensagemBoasVindas();
+ExibirOpcoesDoMenu();
